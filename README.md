7023100fe97185c2d07b4ea1079e44c66a46a4bc
Hukasx0 <hubertkasp13@gmail.com>
2023-06-16 08:32:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# React + Rust project template
+A fullstack project template that can be compiled into a single binary. Template uses:
+- Rust + Actix-web - backend
+- Typescript + React (tsx) - frontend
+- Vite - build
+
+## Usage
+
+### Installation
+```sh
+git clone https://github.com/Hukasx0/actix-react-template
+cd actix-react-template/
+npm i
+```
+you also need Rust and Cargo tools, if you don't have them then install them from [here](https://www.rust-lang.org/learn/get-started)
+
+### start project in watch mode
+
+watch for React changes:
+```sh
+npm run dev
+```
+watch for Rust changes:
+```sh
+npm run dev-rs
+```
+
+## build project
+build React and Rust code into single binary
+```sh
+npm run build-full
+```
+where is this binary after compilation?
+```
+backend/target/release
+```
+
+build only React project
+```sh
+npm run build
+```
---
+++ backend/src/main.rs
@@ -0,0 +1,46 @@
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body(include_str!("../../dist/index.html"))
+}
+
+#[get("/vite.svg")]
+async fn vite_logo() -> HttpResponse {
+    HttpResponse::Ok().content_type("image/svg+xml").body(include_str!("../../dist/vite.svg"))
+}
+
+#[get("/assets/index-4rust.js")]
+async fn js() -> HttpResponse {
+    HttpResponse::Ok().content_type("application/javascript").body(include_str!("../../dist/assets/index-4rust.js"))
+}
+
+#[get("/assets/index-4rust.css")]
+async fn css() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/css").body(include_str!("../../dist/assets/index-4rust.css"))
+}
+
+#[get("/assets/react-4rust.svg")]
+async fn react_logo() -> HttpResponse {
+    HttpResponse::Ok().content_type("image/svg+xml").body(include_str!("../../dist/assets/react-4rust.svg"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let port: u16 = 3000;
+    let hostname: &str = "127.0.0.1";
+
+    println!("Started Rust backend server at:\n -> http://{}:{}/", hostname, port);
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(js)
+            .service(css)
+            .service(vite_logo)
+            .service(react_logo)
+    })
+    .bind((hostname, port))?
+    .run()
+    .await
+}
